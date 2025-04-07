e9a2223169cc65a160bb6f2ab2ea57a7543ce42c
Oleksandr Merlenko <merlenko.business@gmail.com>
2025-04-07 15:08:43 +0200

    add TaskListView into views as homepage

--- todo_app/views.py
+++ todo_app/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from todo_app.models import Task
+
+
+class TaskListView(ListView):
+    model = Task
+    template_name = "todo_app/homepage.html"
+
