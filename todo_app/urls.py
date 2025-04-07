058433f5fdda637407b072ee5f479a0a95954a89
Oleksandr Merlenko <merlenko.business@gmail.com>
2025-04-07 17:09:46 +0200

    add paths for TagListView and complete_task view

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.urls import path
 
-from todo_app.views import TaskListView
+from todo_app.views import TaskListView, TagListView, complete_task
 
 urlpatterns = [
     path("", TaskListView.as_view(), name="home-page"),
+    path("tags/", TagListView.as_view(), name="tag-list"),
+    path("<int:pk>/complete/", complete_task, name="task-complete"),
 ]
 
 app_name = "todo_app"
