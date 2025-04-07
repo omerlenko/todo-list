85d62d087d79feab752ac3418a276b658046457a
Oleksandr Merlenko <merlenko.business@gmail.com>
2025-04-07 18:10:50 +0200

    add paths for new views

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -1,12 +1,20 @@
 from django.contrib import admin
 from django.urls import path
 
-from todo_app.views import TaskListView, TagListView, complete_task
+from todo_app.views import TaskListView, TagListView, complete_task, TaskUpdateView, TaskDeleteView, \
+    TaskCreateView, TagCreateView, TagUpdateView, TagDeleteView
 
 urlpatterns = [
     path("", TaskListView.as_view(), name="home-page"),
-    path("tags/", TagListView.as_view(), name="tag-list"),
+    path("create/", TaskCreateView.as_view(), name="task-create"),
+    path("<int:pk>/update/", TaskUpdateView.as_view(), name="task-update"),
+    path("<int:pk>/delete/", TaskDeleteView.as_view(), name="task-delete"),
     path("<int:pk>/complete/", complete_task, name="task-complete"),
+    path("tags/", TagListView.as_view(), name="tag-list"),
+    path("tags/create/", TagCreateView.as_view(), name="tag-create"),
+    path("tags/<int:pk>/update/", TagUpdateView.as_view(), name="tag-update"),
+    path("tags/<int:pk>/delete/", TagDeleteView.as_view(), name="tag-delete"),
+
 ]
 
 app_name = "todo_app"
